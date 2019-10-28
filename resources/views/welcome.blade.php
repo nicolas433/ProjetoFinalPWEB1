974dd1b2d3958a0b6963a8042f07ad284d9b564a
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-10-28 14:44:23 -0300

    teste2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Meu Ovo
+                    Meu Ovo!
                 </div>
 
                 <div class="links">
