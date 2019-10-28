fca905bc054f6a39a68bbb6b23c6865f5cc13713
nicolas433 <ngrisoste@gmail.com>
2019-10-28 14:33:40 -0300

    Criação das migrations products e categories

---
+++ database/migrations/2019_10_28_171805_create_table_products.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+			$table->float('price');
+            $table->text('description');
+            $table->boolean('active');
+			$table->string('img_url')->nullable();
+            
+            $table->unsignedBigInteger('categories_id');
+            $table->foreign('categories_id')->reference('id')->on('categories');
+ 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
