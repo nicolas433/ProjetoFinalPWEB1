5d88cb91ae7851beda22516593600d8717668e97
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-11-01 13:37:21 -0300

    Instalação do bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Meu Ovo!
+                    Sistema de Delivery!
                 </div>
 
                 <div class="links">
