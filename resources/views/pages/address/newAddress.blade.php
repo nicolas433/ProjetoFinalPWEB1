982ddaf7f4da6d381389b4b2b44aa9d769a4abfb
nicolas433 <ngrisoste@gmail.com>
2019-11-04 14:45:41 -0300

    commit

--- resources/views/pages/address/newAddress.blade.php
+++ resources/views/pages/address/newAddress.blade.php
@@ -13,29 +13,65 @@
                         <form action="/address" method="POST">
                             @csrf
                             <div class="form-group">
-                                <label for="addressName">Nome do produto</label>
+                                <label for="addressName">Numero da casa*</label>
                                 <input
                                     type="text"
                                     class="form-control"
                                     name="addressName"
                                     id="addressName"
-                                    placeholder="Digite o nome do produto"
+                                    placeholder="Digite o numero da casa"
                                 >
                             </div>
                             <div class="form-group">
-                                <label for="addressName">Nome do produto</label>
+                                <label for="addressName">Numero do apartamento</label>
                                 <input
                                     type="text"
                                     class="form-control"
                                     name="addressName"
                                     id="addressName"
-                                    placeholder="Digite o nome do produto"
+                                    placeholder="Digite o numero do apartamento"
                                 >
                             </div>
-
-
-
-
+                            <div class="form-group">
+                                <label for="addressName">Rua*</label>
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    name="addressName"
+                                    id="addressName"
+                                    placeholder="Digite o nome da Rua"
+                                >
+                            </div>
+                            <div class="form-group">
+                                <label for="addressName">Bairro*</label>
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    name="addressName"
+                                    id="addressName"
+                                    placeholder="Digite o nome do Bairro"
+                                >
+                            </div>
+                            <div class="form-group">
+                                <label for="addressName">Cidade*</label>
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    name="addressName"
+                                    id="addressName"
+                                    placeholder="Digite o nome da Cidade"
+                                >
+                            </div>
+                            <div class="form-group">
+                                <label for="addressName">Ponto de referencia</label>
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    name="addressName"
+                                    id="addressName"
+                                    placeholder="Digite um ponto de referencia"
+                                >
+                            </div>
 
                             <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
                             <a href="/addresss" class="btn btn-danger btn-sm">Cancelar</a>
