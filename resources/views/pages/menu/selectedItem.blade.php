301c5672e67a05efec46d519a7eab247d16a3771
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-11-07 12:08:43 -0300

    Criação da estrutura de exibição de um produto escolhido

--- resources/views/pages/menu/selectedItem.blade.php
+++ resources/views/pages/menu/selectedItem.blade.php
@@ -4,8 +4,27 @@
 <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-10">
-                <div class="card">
-                    Detalhes do produto e escolha da quantidade
+                <div class="card p-3">
+                    <div class="desc">
+                        <h4>Nome do produto</h4>
+                        <p>Descrição do produto. asa sasa s ahs ahsahsasa hsahss aasas.</p>
+                        <h4>R$ 2,00</h4>
+                    </div>
+                    <div class="actions d-flex mt-5 justify-content-between">
+                        <p>Quantidade:</p>
+                        <div class="qtd">
+                            <button type="button" class="btn btn-outline-dark btn-sm">-</button>
+                            <span class="mx-2">1</span>
+                            <button type="button" class="btn btn-outline-dark btn-sm">+</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card p-3 fixed-bottom mt-5">
+                    <a class="btn btn-dark text-white btn-sm p-3 d-flex justify-content-between">
+                        <span>Adicionar</span>
+                        <span>R$ YY,YY</span>
+                    </a>
                 </div>
             </div>
         </div>
