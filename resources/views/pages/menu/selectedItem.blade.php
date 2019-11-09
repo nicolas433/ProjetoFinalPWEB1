2648b2c17b0abbdff7fda5b2ae7c27065eded6e7
nicolas433 <ngrisoste@gmail.com>
2019-11-09 15:20:27 -0300

    Pagina selected item completa

--- resources/views/pages/menu/selectedItem.blade.php
+++ resources/views/pages/menu/selectedItem.blade.php
@@ -1,21 +1,23 @@
 @extends('layouts.homeUser')
- 
+
+
+
 @section('content')
-<div class="container">
+<div onload="setValue()" class="container">
         <div class="row justify-content-center">
             <div class="col-md-10">
                 <div class="card p-3">
                     <div class="desc">
                         <h4>{{ $product->name }}</h4>
                         <p>{{ $product->description }}</p>
-                        <h4>R$ {{ $product->price }}</h4>
+                        <h4 style="display: inline" id="total">{{ $product->price }} </h4> <h4 style="display: inline">R$</h4>
                     </div>
                     <div class="actions d-flex mt-5 justify-content-between">
                         <p>Quantidade:</p>
                         <div class="qtd">
-                            <button type="button" class="btn btn-outline-dark btn-sm">-</button>
-                            <span class="mx-2">1</span>
-                            <button type="button" class="btn btn-outline-dark btn-sm">+</button>
+                            <button type="button" class="btn btn-outline-dark btn-sm" onClick="decrementar('valor')">-</button>
+                            <span id="valor" class="mx-2">1</span>
+                            <button type="button" class="btn btn-outline-dark btn-sm" onClick="incrementar('valor')">+</button>
                         </div>
                     </div>
                 </div>
@@ -24,15 +26,65 @@
                     <form action="/shoppingbag" method="POST">
                         @csrf
                         <input type="hidden" name="productId" value="{{ $product->id }}">
-                        <input type="hidden" name="amount" value="2">
-                        <input type="hidden" name="totalValue" value="10">
+                        <input id="subamount" type="hidden" name="amount" value="1">
+                        <input id="subvalue" type="hidden" name="totalValue" value="{{ $product->price }}">
                         <button type="submit" class="btn btn-dark text-white btn-sm p-3 d-flex justify-content-between">
-                            <span>Adicionar</span>
-                            <span>R$ YY,YY</span>
+                            <span>Adicionar &nbsp </span>
+                            <span id="sub">{{ $product->price }} </span><span>R$</span>
                         </button>
                     </form>
                 </div>
             </div>
         </div>
-    </div>          
+    </div>  
+    <script type="text/javascript">
+
+        const preço = parseInt(total.innerText);
+
+        function setValue(){
+            let subvalue = document.getElementById("subvalue");
+            subvalue.value = preço;
+        }
+
+        function incrementar(valor) {          
+            let contador = document.getElementById(valor);
+            let total = document.getElementById("total");
+            let sub = document.getElementById("sub");
+          
+
+            contador.innerHTML = parseInt(contador.innerText) + 1;
+            total.innerHTML = parseInt(total.innerText) + preço;
+            sub.innerHTML = parseInt(sub.innerText) + preço;
+
+            let subvalue = document.getElementById("subvalue");
+            let subamount = document.getElementById("subamount");  
+            subvalue.value = parseInt(total.innerText);
+            subamount.value = parseInt(contador.innerText);
+        }
+
+        function decrementar(valor) {
+            let contador = document.getElementById(valor);
+            let total = document.getElementById("total");
+
+            if (parseInt(total.innerText) == preço) {
+                total.innerText = preço;
+                sub.innerHTML = preço;
+            } else {
+                total.innerHTML = parseInt(total.innerText) - preço;
+                sub.innerHTML = parseInt(sub.innerText) - preço;
+            }
+
+            if (contador.innerText == '1') {
+                contador.innerText = '1'
+            } else {
+                contador.innerHTML = parseInt(contador.innerText) - 1;
+            }
+
+            let subvalue = document.getElementById("subvalue");
+            let subamount = document.getElementById("subamount");  
+            subvalue.value = parseInt(total.innerText);
+            subamount.value = parseInt(contador.innerText);
+        }
+    </script>
 @endsection
+
