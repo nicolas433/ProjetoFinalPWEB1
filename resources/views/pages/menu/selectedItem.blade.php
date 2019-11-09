e6c56a3f026c9766514e84fbbe0b69520872580a
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-11-09 08:58:51 -0300

    Criação da exibição dos dados do item selecionado

--- resources/views/pages/menu/selectedItem.blade.php
+++ resources/views/pages/menu/selectedItem.blade.php
@@ -6,9 +6,9 @@
             <div class="col-md-10">
                 <div class="card p-3">
                     <div class="desc">
-                        <h4>Nome do produto</h4>
-                        <p>Descrição do produto. asa sasa s ahs ahsahsasa hsahss aasas.</p>
-                        <h4>R$ 2,00</h4>
+                        <h4>{{ $product->name }}</h4>
+                        <p>{{ $product->description }}</p>
+                        <h4>R$ {{ $product->price }}</h4>
                     </div>
                     <div class="actions d-flex mt-5 justify-content-between">
                         <p>Quantidade:</p>
@@ -21,10 +21,14 @@
                 </div>
 
                 <div class="card p-3 fixed-bottom mt-5">
-                    <a class="btn btn-dark text-white btn-sm p-3 d-flex justify-content-between">
-                        <span>Adicionar</span>
-                        <span>R$ YY,YY</span>
-                    </a>
+                    <form>
+                        <input type="hidden" name="qtd">
+                        <input type="hidden" name="total-value">
+                        <a class="btn btn-dark text-white btn-sm p-3 d-flex justify-content-between">
+                            <span>Adicionar</span>
+                            <span>R$ YY,YY</span>
+                        </a>
+                    </form>
                 </div>
             </div>
         </div>
