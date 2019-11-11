6a511cd3f8a50a46d2b8df6d4c3d1569ad1806ed
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-11-11 09:52:26 -0300

    Adição de link para realizar novo pedido

--- resources/views/pages/myrequests/requestDone.blade.php
+++ resources/views/pages/myrequests/requestDone.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-10">
                 <h1>Seu pedido foi realizado com sucesso!!</h1>
                 <a href="/requests" class="btn btn-dark btn-sm">Acompanhar pedido</a>
-                <a href="" class="btn btn-dark btn-sm">Fazer novo pedido</a>
+                <a href="/menu/categories" class="btn btn-dark btn-sm">Fazer novo pedido</a>
             </div>
         </div>
     </div>
