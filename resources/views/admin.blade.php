deb6aa850222578d03a213b111a9e7e1de97fa3c
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-11-14 09:59:45 -0300

    Ajuste na exibição de novos pedidos

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -24,20 +24,26 @@
             },
             function(data, status){
                 if(data != currentData){
+                    if (currentData != null) {
+                        $('#container').empty();
+                    }
+
                     currentData = data
 
                     var newData = JSON.parse(currentData);
 
                     newData.forEach((elem, index)=>{
-                        console.log(elem);
-                        $('#container').append(`<div class='card p-3 mb-3' id='card${elem.id}'></div>`);
-                        $(`#card${elem.id}`).append(`<h3>Novo pedido</h3>`);
-                        $(`#card${elem.id}`).append(`<p>ID: ${elem.id}</p>`);
-                        $(`#card${elem.id}`).append(`<p>Cliente: ${elem.user}</p>`);
-                        $(`#card${elem.id}`).append(`<p>Data de realização do pedido: ${elem.created_at}</p>`);
-                        $(`#card${elem.id}`).append(`<p>Status do pedido: ${elem.status}</p>`);
-                        $(`#card${elem.id}`).append(`<a href='/admin/requests/request/${elem.id}' class='btn btn-dark btn-sm'>Ver</a>`);
+                        if(elem.id != null){
+                            $('#container').append(`<div class='card p-3 mb-3' id='card${elem.id}'></div>`);
+                            $(`#card${elem.id}`).append(`<h3>Novo pedido</h3>`);
+                            $(`#card${elem.id}`).append(`<p>ID: ${elem.id}</p>`);
+                            $(`#card${elem.id}`).append(`<p>Cliente: ${elem.user}</p>`);
+                            $(`#card${elem.id}`).append(`<p>Data de realização do pedido: ${elem.created_at}</p>`);
+                            $(`#card${elem.id}`).append(`<p>Status do pedido: ${elem.status}</p>`);
+                            $(`#card${elem.id}`).append(`<a href='/admin/requests/request/${elem.id}' class='btn btn-dark btn-sm'>Ver</a>`);
+                        }
                     });
+
                 } else {
                     console.log('iguais');
                 }
