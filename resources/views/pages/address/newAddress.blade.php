229053f74b5f4dd47a977814c0638eb9be1e1dcb
jessicamaia <51716788+jessicamaia@users.noreply.github.com>
2019-11-14 14:22:57 -0300

    Update newAddress.blade.php
    Adição dos estilos dos inputs.

--- resources/views/pages/address/newAddress.blade.php
+++ resources/views/pages/address/newAddress.blade.php
@@ -18,7 +18,7 @@
                                 <label for="houseNumber">Numero da casa*</label>
                                 <input
                                     type="text"
-                                    class="form-control"
+                                    class="my-form-control"
                                     name="houseNumber"
                                     id="houseNumber"
                                     placeholder="Digite o numero da casa"
@@ -28,7 +28,7 @@
                                 <label for="apartmentNumber">Numero do apartamento</label>
                                 <input
                                     type="text"
-                                    class="form-control"
+                                    class="my-form-control"
                                     name="apartmentNumber"
                                     id="apartmentNumber"
                                     placeholder="Digite o numero do apartamento"
@@ -38,7 +38,7 @@
                                 <label for="street">Rua*</label>
                                 <input
                                     type="text"
-                                    class="form-control"
+                                    class="my-form-control"
                                     name="street"
                                     id="street"
                                     placeholder="Digite o nome da Rua"
@@ -48,7 +48,7 @@
                                 <label for="neighborhood">Bairro*</label>
                                 <input
                                     type="text"
-                                    class="form-control"
+                                    class="my-form-control"
                                     name="neighborhood"
                                     id="neighborhood"
                                     placeholder="Digite o nome do Bairro"
@@ -58,7 +58,7 @@
                                 <label for="city">Cidade*</label>
                                 <input
                                     type="text"
-                                    class="form-control"
+                                    class="my-form-control"
                                     name="city"
                                     id="city"
                                     placeholder="Digite o nome da Cidade"
@@ -68,7 +68,7 @@
                                 <label for="referencePoint">Ponto de referencia</label>
                                 <input
                                     type="text"
-                                    class="form-control"
+                                    class="my-form-control"
                                     name="referencePoint"
                                     id="referencePoint"
                                     placeholder="Digite um ponto de referencia"
