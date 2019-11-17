e7fb230c96cd5ef6064815aa6613648322131846
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-11-17 12:40:35 -0300

    Criação da seed para cadastro de admin e status

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,41 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12345678'),
+            'is_admin' => '1'
+        ]);
+
+        DB::table('status')->insert([
+            'title' => 'Aguardando confirmação',
+            'description' => 'Aguardando a confirmação do restaurante',
+        ]);
+
+        DB::table('status')->insert([
+            'title' => 'Pedido confirmado',
+            'description' => 'O pedido já foi aceito pelo restaurante e esta sendo preparado',
+        ]);
+
+        DB::table('status')->insert([
+            'title' => 'Pedido enviado',
+            'description' => 'Seu pedido já está sendo enviado para seu endereço',
+        ]);
+
+        DB::table('status')->insert([
+            'title' => 'Pedido cancelado',
+            'description' => 'O cliente cancelou o pedido',
+        ]);
+
+        DB::table('status')->insert([
+            'title' => 'Pedido recusado',
+            'description' => 'O restaurante não pode concluir seu pedido',
+        ]);
+
+        DB::table('status')->insert([
+            'title' => 'Pedido entregue',
+            'description' => 'Entraga concluída',
+        ]);
     }
 }
